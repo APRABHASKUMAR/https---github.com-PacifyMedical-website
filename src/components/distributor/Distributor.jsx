d01c33608e60bb6d12b90791b1c7218a49a7c5e8
prabhas <apk543211@gmail.com>
2023-07-10 15:05:53 +0530

    post api-5

--- src/components/distributor/Distributor.jsx
+++ src/components/distributor/Distributor.jsx
@@ -4,7 +4,7 @@ import "./distributor.css";
 const baseUrl = 'https://saishm.pythonanywhere.com';
 const Distributor = () => {
   const [formData, setFormData] = React.useState(
-    {
+    { title: "",
       description: "", 
       personalname: "", 
       personalemail: "", 
